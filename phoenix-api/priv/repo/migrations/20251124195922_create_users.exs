243d0b71f44aaebd44b748bafe48448d893a14bd
Rafał Międziocha <Rafalsky@users.noreply.github.com>
2025-12-05 11:08:13 +0100

    feat: create users table with essential fields and indexes

--- phoenix-api/priv/repo/migrations/20251124195922_create_users.exs
+++ phoenix-api/priv/repo/migrations/20251124195922_create_users.exs
@@ -2,7 +2,8 @@ defmodule PhoenixApi.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
       add :first_name, :string
       add :last_name, :string
       add :birthdate, :date
@@ -10,5 +11,10 @@ defmodule PhoenixApi.Repo.Migrations.CreateUsers do
 
       timestamps(type: :utc_datetime)
     end
+
+    create index(:users, [:first_name])
+    create index(:users, [:last_name])
+    create index(:users, [:birthdate])
+    create index(:users, [:gender])
   end
 end
